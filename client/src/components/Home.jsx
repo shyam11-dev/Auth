2c6af00dff4d7e07f591206f529e6da263eb2a20
shyam11-dev <morishyam11112005@gmail.com>
2026-05-19 10:44:54 +0530

    list part ready

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,46 +1,85 @@
 function Home() {
   return (
     <div className="w-full px-5 min-h-[calc(100vh-60px)]">
-      <div className="w-full grid grid-cols-5">
-        <div>
+      <div className="w-full grid grid-cols-5 gap-4 my-4">
+        <div className="w-full flex flex-col gap-2">
           <label htmlFor="">Book Name :</label>
           <input
             type="text"
             placeholder="Book Name"
-            className="w-ful border border-gray-100 rounded-sm outline-1"
+            className="w-ful border border-gray-500 rounded-sm h-8 px-2"
           />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label htmlFor="">Book title :</label>
           <input
             type="text"
             placeholder="Book Title"
-            className="w-ful border border-gray-100 rounded-sm outline-1"
+            className="w-ful border border-gray-500 rounded-sm h-8 px-2"
           />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label htmlFor="">Author :</label>
           <input
             type="text"
             placeholder="Book Author"
-            className="w-ful border border-gray-100 rounded-sm outline-1"
+            className="w-ful border border-gray-500 rounded-sm h-8 px-2"
           />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label htmlFor="">Selling Price :</label>
           <input
             type="text"
             placeholder="Selling price"
-            className="w-ful border border-gray-100 rounded-sm outline-1"
+            className="w-ful border border-gray-500 rounded-sm h-8 px-2"
           />
         </div>
-        <div>
+        <div className="w-full flex flex-col gap-2">
           <label htmlFor="">Publish Date :</label>
           <input
             type="date"
             placeholder="Publish Date"
-            className="w-ful border border-gray-100 rounded-sm outline-1"
+            className="w-ful border border-gray-500 rounded-sm h-8 px-2"
           />
+        </div>
+      </div>
+      <div className="w-full flex justify-end">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-sm">
+          Add Book
+        </button>
+      </div>
+      <div className="mt-10 w-full">
+        <div className="w-full">
+          <table className="w-full bg-white divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="tracing-wider px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                  Book Name
+                </th>
+                <th className="tracing-wider px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                  Book Title
+                </th>
+                <th className="tracing-wider px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                  Author
+                </th>
+                <th className="tracing-wider px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                  Selling Price
+                </th>
+                <th className="tracing-wider px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase">
+                  Publish Date
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              <tr className="hover:bg-gray-200">
+                <td className="px-6 py-4 whitespace-nowrap">Book 1</td>
+                <td className="px-6 py-4 whitespace-nowrap">Title 1</td>
+                <td className="px-6 py-4 whitespace-nowrap">Author 1</td>
+                <td className="px-6 py-4 whitespace-nowrap">Price 1</td>
+                <td className="px-6 py-4 whitespace-nowrap">Date 1</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
